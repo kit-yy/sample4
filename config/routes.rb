d57641cfe2bde62da5db267a7b50e28e6bd222e5
USER <user@USER-no-MacBook-Pro.local>
2016-11-05 12:22:56 +0900

    chap7 start

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
 
   root             'static_pages#home'
@@ -7,4 +9,9 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   resources :users
+
+  get 'login' => 'session#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'sessions#destroy'
+  
 end
