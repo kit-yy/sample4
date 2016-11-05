55a0ede12fcf49ce9b8ae3b5c1d45e1c8ea6fe4b
USER <user@USER-no-MacBook-Pro.local>
2016-11-05 12:52:07 +0900

    chap8-1 end

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'login' => 'session#new'
-  post 'login' => 'session#create'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-  
+
 end
