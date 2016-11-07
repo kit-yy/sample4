684b1f5d0626ce7c9f5a7dccfd385b6045477a7f
USER <user@USER-no-MacBook-Pro.local>
2016-11-07 16:00:51 +0900

    chap8.4.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -16,8 +20,18 @@ class User < ActiveRecord::Base
   end
   # digestの計算は、ユーザ毎に行われるため、user.rbに配置。
 
-  def User.new_token
+　 def User.new_token　#ランダムなトークンを返すメソッド。
+  #パスワードではなく、cookies版のdigestメソッドのようなもの。
+  #ということで、同様にuser.rbで定義。
     SecureRandom.urlsafe_base64
   end
 
+  # 永続的セッションで使用するユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    # クラス利用のselfではなく、インスタンス利用のself。
+    #このselfはこの関数が含まれている「Userクラス」のインスタンスに置き換えられる
+    #こう定義しないと、Rubyが勝手にremember_tokenというローカル変数を作ってしまう。
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
